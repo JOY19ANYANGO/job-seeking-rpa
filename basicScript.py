dd094157481dda8355694bc4865577447608bcf7
JOY19ANYANGO <joyanyango402@gmail.com>
2023-12-30 17:19:29 +0300

    made a few improvements

--- basicScript.py
+++ basicScript.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 options = Options()
 # options.add_argument('--headless')
@@ -11,4 +13,9 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 driver.get("https://www.google.com/")
 print("Page Title:", driver.title)
+object_name = driver.find_element(By.CLASS_NAME,"RNNXgb")
+# object_name.send_keys("test")
+# object_name.send_keys(Keys.RETURN)
+print("Page Title:", driver.page_source)
+
 driver.quit()
